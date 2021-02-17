339fa5da371bcbf5a986509756e1c9a71c3fc364
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-17 18:27:39 +0300

    final commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tensorflow.keras.layers import Dense, Dropout, LSTM
 # Load Data
 company = 'FB'
 start = dt.datetime(2012, 1, 1)
-end = dt.datetime(2020, 1, 1)
+end = dt.datetime(2021, 2, 17)
 
 data = web.DataReader(company, 'yahoo', start, end)
 
@@ -18,7 +18,7 @@ data = web.DataReader(company, 'yahoo', start, end)
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
 
-prediction_days = 60
+prediction_days = 365
 x_train = []
 y_train = []
 
@@ -41,17 +41,17 @@ model.add(Dropout(0.2))
 model.add(Dense(units=1))  # Prediction of next closing value
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=25, batch_size=32)
+model.fit(x_train, y_train, epochs=100, batch_size=32)
 
 '''Test the Model Accuracy on Existing Data'''
 # Load Test Data
-test_start = dt.datetime(2020, 1,1)
+test_start = dt.datetime(2020, 1, 1)
 test_end = dt.datetime.now()
 
 test_data = web.DataReader(company, 'yahoo', test_start, test_end)
 actual_prices = test_data['Close'].values
 
-total_dataset = pd.concat((data['CLose'], test_data['Close']), axis=0)
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 model_inputs = total_dataset[(len(total_dataset) - len(test_data) - prediction_days):].values
 model_inputs = model_inputs.reshape(-1, 1)
 model_inputs = scaler.transform(model_inputs)
@@ -63,7 +63,7 @@ for x in range(prediction_days, len(model_inputs)):
     x_test.append(model_inputs[x - prediction_days:x, 0])
 
 x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1]), 1)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
@@ -77,3 +77,12 @@ plt.ylabel(f'{company} Share Price')
 plt.legend()
 plt.show()
 
+# Predict Next Day
+
+real_data  =[model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs + 1), 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f"Prediction: {prediction}")
