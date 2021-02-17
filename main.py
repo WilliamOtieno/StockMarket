c07184d32c7bc74f4060a671d7b6c4aac8c7a8ab
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-17 16:47:34 +0300

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
